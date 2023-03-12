3c42416a6e7eac4376d0c457c71dbae74452b3da
Maksym Kuzmych <kuzmichmaksym1997@gmail.com>
2023-03-12 11:40:21 +0200

    refactor: clean code

--- src/js/utilites/localStorage.js
+++ src/js/utilites/localStorage.js
@@ -8,33 +8,36 @@ export function setLocalStorage(arr, size, moves, sec, min) {
       sec,
       min,
     })
-  )
+  );
 }
 
 export function setLocalStorageResults(result) {
   if (JSON.parse(localStorage.getItem('results'))) {
-    let res = JSON.parse(localStorage.getItem('results'))
+    const res = JSON.parse(localStorage.getItem('results'));
+
     if (res.length < 10) {
-      res.unshift(result)
+      res.unshift(result);
     } else {
-      res.pop()
-      res.unshift(result)
+      res.pop();
+      res.unshift(result);
     }
-    localStorage.setItem('results', JSON.stringify(res))
+
+    localStorage.setItem('results', JSON.stringify(res));
   } else {
-    localStorage.setItem('results', JSON.stringify([result]))
+    localStorage.setItem('results', JSON.stringify([result]));
   }
 }
 
 export function getLocalStorage(value) {
   if (JSON.parse(localStorage.getItem('game'))) {
-    let obj = JSON.parse(localStorage.getItem('game'))
-    return obj[value]
+    const obj = JSON.parse(localStorage.getItem('game'));
+
+    return obj[value];
   }
 }
 
 export function getLocalStorageResults() {
   if (JSON.parse(localStorage.getItem('results'))) {
-    return JSON.parse(localStorage.getItem('results'))
+    return JSON.parse(localStorage.getItem('results'));
   }
 }
